66b0c128615642543245ae1531f367d2f0560853
Ubuntu <ubuntu@ip-172-31-12-236.ec2.internal>
2014-10-13 18:54:37 +0000

    rendering a version of the scribble in png to experiment with the laser cutting

---
+++ svg2png.php
@@ -0,0 +1,17 @@
+<?php 
+
+$svg = $_REQUEST['svg'];
+
+$im = new Imagick();
+$im->setBackgroundColor(new ImagickPixel('transparent'));
+$im->readImageBlob($svg);
+
+
+/*png settings*/
+$im->setImageFormat("png24");
+header('Content-Type: image/png');
+echo $im;
+$im->clear();
+$im->destroy();
+
+?>
