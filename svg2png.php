006a55a05228f35e7a7ee86c656fa0df766ac514
Ubuntu <ubuntu@ip-172-31-12-236.ec2.internal>
2014-10-30 19:25:01 +0000

    accepts guid for converting to png

--- svg2png.php
+++ svg2png.php
@@ -1,6 +1,7 @@
 <?php 
 
-$svg = $_REQUEST['svg'];
+$guid = $_REQUEST["id"];
+$svg = file_get_contents("https://s3.amazonaws.com/curliceu/" . $guid . ".svg");
 
 $im = new Imagick();
 $im->setBackgroundColor(new ImagickPixel('transparent'));
