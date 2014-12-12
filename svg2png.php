c3bc7a6441fdf50167655a555c84e9430a90e3d1
Ubuntu <ubuntu@ip-172-31-12-236.ec2.internal>
2014-12-12 15:15:23 +0000

    smooth curves!

--- svg2png.php
+++ svg2png.php
@@ -1,18 +1,16 @@
 <?php 
 
-$guid = $_REQUEST["id"];
-$svg = file_get_contents("https://s3.amazonaws.com/curliceu/" . $guid . ".svg");
+$id = $_REQUEST["id"];
+$svg = file_get_contents("https://s3.amazonaws.com/curliceu/" . $id . ".svg");
 
-$im = new Imagick();
-$im->setBackgroundColor(new ImagickPixel('transparent'));
-$im->readImageBlob($svg);
-
-
-/*png settings*/
-$im->setImageFormat("png24");
 header('Content-Type: image/png');
-echo $im;
-$im->clear();
-$im->destroy();
+$r = new Rsvg($svg);
+$s = new CairoImageSurface(CairoFormat::ARGB32, 1000, 1000);
+$c = new CairoContext($s);
+$r->render($c);
+$filename = "/tmp/" . $id . ".php";
+$s->writeToPng($filename);
+echo file_get_contents($filename);
+unlink($filename);
 
 ?>
