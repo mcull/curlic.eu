b1232d577451e1fa38150e3df7b0de8cf529a3e9
Marc Cull <marc.cull@gmail.com>
2014-12-22 04:02:30 -0500

    saving orders in DynamoDB

--- order/index.php
+++ order/index.php
@@ -4,6 +4,7 @@ require_once('./Stripe.php');
 require '../aws/aws-autoloader.php';
 require '../credentials.php';
 use Aws\Ses\SesClient;
+use Aws\DynamoDb\DynamoDbClient;
 
 Stripe::setApiKey("UmSEWvgAWiTGgQnWnfFMTdVRCEh8mSma");
 
@@ -13,7 +14,7 @@ $price = $_POST['price'];
 $email = $_POST['billingEmail'];
 
 $charge = null;
-$e = null;
+$error = null;
 
 
 
@@ -21,7 +22,13 @@ $fonts = json_decode(file_get_contents('../fonts.json'),true);
 $ttfFont = getFontByName($_POST["font"]);
 
 $graphicUrl = "http://www.curlic.eu/namesvg/?text=" 
-			. $_POST["text"] . "&font=" . $ttfFont;
+			. $_POST["text"] 
+			. "&font=" 
+			. $ttfFont["ttfName"] 
+			. "&stroke=" 
+			. ((ISSET($ttfFont["stroke"]) && strlen($ttfFont["stroke"]) > 0) ? $ttfFont["stroke"] : "0")
+			. "&spacing=" 
+			. ((ISSET($ttfFont["spacing"]) && strlen($ttfFont["spacing"]) > 0) ? $ttfFont["spacing"] : "0");
 
 date_default_timezone_set("America/New_York");
 
@@ -40,19 +47,58 @@ try {
 	  "description" => "Charge for Curliceu")
 	);
 	sendEmailToPrinter($charge);
+	persistOrder($charge);
 
 } catch(Stripe_CardError $e) {
   // The card has been declined
-	echo $e;
+	$error =  $e;
 }
 
 function getFontByName($name) {
 	global $fonts;
 	foreach ($fonts as $font) {
 		if (str_replace("-"," ",$name) == $font["name"])  {
-			return $font["ttfName"];
+			return $font;
 		}
 	}
+}
+
+function persistOrder($charge) {
+	global $key,$secret;
+	$client = DynamoDbClient::factory(array(
+    	'key' => $key,
+    	'secret'=>$secret,
+    	'region'  => 'us-east-1'
+	));
+
+	$result = $client->putItem(array(
+    'TableName' => 'curliceu_orders',
+    'Item' => array(
+        'id'      			=> array('S' => $charge["id"]),
+        'order_date'    			=> array('N' => time()),
+        'amount'			=> array('N' => $charge["amount"]/100),
+        'text'				=> array('S' => $_POST['text']),
+        'font'				=>  array('S' => $_POST['font']),
+        'material'			=> array('S' => $_POST['material']),
+        'cardBrand'			=> array('S' => $charge['card']['brand']),
+        'last4'				=> array('S' => $charge['card']['last4']),
+        'billName'			=> array('S' => $_POST['billName']),
+        'billAddress1'		=> array('S' => $_POST['billAddress1']),
+        'billAddress2'		=> array('S' => (ISSET($_POST['billAddress2']) && strlen($_POST['billAddress2'] > 0) ? $_POST['billAddress2'] : "none")),
+        'billCity'			=> array('S' => $_POST['billCity']),
+        'billState'			=> array('S' => $_POST['billState']),
+        'billZip'			=> array('S' => $_POST['billZip']),
+        'shippingMethod'	=> array('S' => $_POST['shippingMethod']),
+        'shipName'			=> array('S' => $_POST['shipName']),
+        'shipAddress1'		=> array('S' => $_POST['shipAddress1']),
+        'shipAddress2'		=> array('S' => (ISSET($_POST['shipAddress2']) && strlen($_POST['shipAddress2'] > 0) ? $_POST['shipAddress2'] : "none")),
+        'shipCity'			=> array('S' => $_POST['shipCity']),
+        'shipState'			=> array('S' => $_POST['shipState']),
+        'shipZip'			=> array('S' => $_POST['shipZip']),
+        'customer_email'	=> array('S' => $_POST['billEmail']),
+		'customer_sms'		=> array('S' => (ISSET($_POST['billSMS']) && strlen($_POST['billSMS'] > 0) ? $_POST['billSMS'] : "none"))
+		)
+	));
 }
 
 function sendEmailToPrinter($charge) {
@@ -164,8 +210,27 @@ function sendEmailToPrinter($charge) {
   </head>
   <body style="font-family:'Quicksand', sans-serif;font-size:.75rem;width:80%;margin:0px auto;">
   	<?php
-  	 	//echo $charge;
-		//echo $charge['card']['last4'];
+  	 	if ($error) {
+  	?>
+
+  	 <div class="row">
+  		<div class="large-12 columns" style="padding-top:1rem;padding-bottom:2rem;">
+  			Sorry!!  We could not process your payment:
+  			<span style="color:red;font-weight:bold">
+  			 <?php 	$body = $e->getJsonBody();
+  					$err  = $body['error'];
+  					echo $err['message'];
+ 			?></span>  
+ 			<p>Please go back to
+  			try your order again.  
+
+  			<p>For questions, 
+  			contact us anytime at <a href="mailto:help@curlic.eu">help@curlic.eu</a>.
+  		</div>
+  	</div>
+
+  	<?php 
+  	 	} else {
 	?>
   	<div class="row">
   		<div class="large-12 columns" style="text-align:center;font-size:2rem;font-family:Rochester;padding-top:2rem;">
@@ -238,7 +303,7 @@ function sendEmailToPrinter($charge) {
 
   		</div>
   	</div>
-
+  	<?php } ?>
   	<div class="row" id="footer"> 
 	  <div class="large-12 columns" style="text-align:center;">
 	    <h6><small>Hand-made with <i class="fa fa-heart"></i> in Kentucky  &copy;2014 Curliceu <a href="privacy.html">Privacy Policy</a></small></h6>
